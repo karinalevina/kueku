e923e46e05b90a71543ed859078e7351f7a45e46
karinalevina <karina_14@kharisma.ac.id>
2017-08-07 09:20:18 +0800

    gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -20,7 +20,7 @@
 //$ipadd = 'http://10.0.4.99';
 //$ipadd = 'http://10.0.4.221/';
 //$ipadd = 'http://10.0.4.54/';
-$config['base_url']	= 'http://karina-ta.kharisma.ac.id/';
+$config['base_url']	= 'http://kueku.local/';
 //$ipadd.'/kueku//';
 
 /*
